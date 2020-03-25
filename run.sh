d7568f52ee7425d4dc00b0dc9c4560208025ebca
Ionut Radu <ionut.radu@tremend.com>
2020-03-25 14:09:44 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+function send_message(){
+
+  local BOT_MESSAGE="$1"
+  local BOT_GROUP_ID="$2"
+  local BOT_URL="$3"
+
+
+  if [[ -n ${BOT_GROUP_ID} ]] && [[ -n ${BOT_URL} ]];then
+    echo -e "{\n}" > bot_body.json
+    cat bot_body.json |
+    jq 'setpath(["groupId"]; "${BOT_GROUP_ID}")'|
+    jq 'setpath(["message"]; "${BOT_MESSAGE}")' > bot_body_send.json
+    RESPONSE_CODE=$(curl --write-out %{http_code} --silent --output /dev/null -X POST --data @bot_body_send.json ${BOT_URL} -H "Content-Type: application/json")
+    echo ${RESPONSE_CODE}
+  fi
+}
+
+
+send_message "${WERCKER_SEND_MESSAGE_BOT_MESSAGE}" "${WERCKER_SEND_MESSAGE_BOT_GROUP_ID}" "${WERCKER_SEND_MESSAGE_BOT_URL}"
