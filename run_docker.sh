9cb0b157b58b1b4497aa6b1da411e4acf8bce383
Ionut Radu <ionut.radu@tremend.com>
2021-02-24 11:56:16 +0200

    add slack integration

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-if [[ -n ${BOT_GROUP_ID} ]] && [[ -n ${BOT_URL} ]] && [[ -n ${BOT_MESSAGE} ]];then
+
+if [[ ${BOT_APP,,} == "slack" ]];then
+  BOT_USERNAME=${BOT_USERNAME:"SlackBot"}
+  BOT_EMOJI=${BOT_EMOJI:"ghost"}
+  if [[ -n ${BOT_URL} ]] && [[ -n ${BOT_MESSAGE} ]];then
+    PAYLOAD=$(echo -e "{\n}" |
+    jq 'setpath(["username"]; "'"${BOT_USERNAME}"'")'|
+    jq 'setpath(["text"]; "'"${BOT_MESSAGE}"'")' |
+    jq 'setpath(["icon_emoji"]; "'"${BOT_EMOJI}"'")' )
+    RESPONSE_CODE=$(curl -k --write-out %{http_code} --silent --output /dev/null -X POST --data-urlencode "payload=$PAYLOAD" ${BOT_URL} )
+    echo "Slack response_conde: ${RESPONSE_CODE} "
+  fi
+elif [[ -n ${BOT_GROUP_ID} ]] && [[ -n ${BOT_URL} ]] && [[ -n ${BOT_MESSAGE} ]];then
   echo -e "{\n}" > bot_body.json
   cat bot_body.json |
   jq 'setpath(["groupId"]; "'"${BOT_GROUP_ID}"'")'|
