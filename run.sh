191d8bb245164ae2c9938a2ec9191f66afd8afe8
Ionut Radu <ionut.radu@tremend.com>
2021-02-24 12:42:49 +0200

    wercker: slack notify

--- run.sh
+++ run.sh
@@ -7,8 +7,18 @@ function send_message(){
   local BOT_GROUP_ID="$2"
   local BOT_URL="$3"
 
-
-  if [[ -n ${BOT_GROUP_ID} ]] && [[ -n ${BOT_URL} ]];then
+  if [[ ${BOT_APP,,} == "slack" ]];then
+    BOT_USERNAME=${BOT_USERNAME:"SlackBot"}
+    BOT_EMOJI=${BOT_EMOJI:"ghost"}
+    if [[ -n ${BOT_URL} ]] && [[ -n ${BOT_MESSAGE} ]];then
+      PAYLOAD=$(echo -e "{\n}" |
+      jq 'setpath(["username"]; "'"${BOT_USERNAME}"'")'|
+      jq 'setpath(["text"]; "'"${BOT_MESSAGE}"'")' |
+      jq 'setpath(["icon_emoji"]; "'"${BOT_EMOJI}"'")' )
+      RESPONSE_CODE=$(curl -k --write-out %{http_code} --silent --output /dev/null -X POST --data-urlencode "payload=$PAYLOAD" ${BOT_URL} )
+      echo "Slack response_conde: ${RESPONSE_CODE} "
+    fi
+  elif [[ -n ${BOT_GROUP_ID} ]] && [[ -n ${BOT_URL} ]];then
     echo -e "{\n}" > bot_body.json
     cat bot_body.json |
     jq 'setpath(["groupId"]; "'"${BOT_GROUP_ID}"'")'|
